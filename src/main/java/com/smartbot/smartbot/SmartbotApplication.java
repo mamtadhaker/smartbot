c5ae54c5178bd01d7fcefb6523a0a928da8201e3
Mamta Dhaker <mamta.dhaker28@gmail.com>
2017-09-30 01:04:33 +0530

    initial commit

---
+++ src/main/java/com/smartbot/smartbot/SmartbotApplication.java
@@ -0,0 +1,12 @@
+package com.smartbot.smartbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbotApplication.class, args);
+	}
+}
